ea727eb94a8bc925ad2c53bd5cf76c8f79221b5f
padamsarda <padam.sarda.tech@gmail.com>
2026-03-26 15:11:47 +0530

    Add V1 basic pipeline STM32 project

---
+++ v1_basic_pipeline/daq_pipeline/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/padam/STM32CubeIDE/workspace_2.0.0/stm32cubef4-v1-28-3/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
